1bbe105b9032159bf655e25b58cc3a3cc82cde89
Ramandeep <raman@localhost.localdomain>
2017-07-12 10:25:48 +0530

    initial project layout created

---
+++ src/main/java/com/qainfotech/tap/frameworks/webservices/WebServiceClient.java
@@ -0,0 +1,10 @@
+package com.qainfotech.tap.frameworks.webservices;
+
+/**
+ *
+ * @author Ramandeep RamandeepSingh AT QAInfoTech.com
+ */
+public interface WebServiceClient {
+    
+    
+}
---
+++ src/test/java/com/qainfotech/tap/frameworks/webservices/RestClientTest.java
@@ -0,0 +1,12 @@
+package com.qainfotech.tap.frameworks.webservices;
+
+/**
+ *
+ * @author Ramandeep RamandeepSingh AT QAInfoTech.com
+ */
+public class RestClientTest {
+    
+    public RestClientTest() {
+    }
+    
+}
